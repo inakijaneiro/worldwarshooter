080543d54d39134ef7b472d2d87d729000f15577
Rigoberto Valadez <rigovaladezmena@hotmail.com>
2019-05-08 13:45:27 -0500

    Added the boss

--- WWShooter/src/wwshooter/Tank.java
+++ WWShooter/src/wwshooter/Tank.java
@@ -15,11 +15,15 @@ public class Tank extends Item{
     
     int health;
     Animation animation;
+    Level level;
+    int lastShot;
     
-    public Tank(int x, int y, int width, int height) {
+    public Tank(int x, int y, int width, int height, Level level) {
         super(x, y, width, height);
         this.health = 10;
         this.animation = new Animation(Assets.tank, 60);
+        this.level = level;
+        this.lastShot = 0;
     }
 
     public int getHealth() {
@@ -29,10 +33,26 @@ public class Tank extends Item{
     public void setHealth(int health) {
         this.health = health;
     }
-    
+
+    public Level getLevel() {
+        return level;
+    }
+        
     @Override
     public void tick() {
         animation.tick();
+        if(lastShot > 60){
+            getLevel().getRockets().push(new Bullet(getX() + getWidth()/2, getY() + (int)(getHeight()*0.45), 32, 16, 7, getLevel(), Bullet.Direction.LEFT, 'r'));
+            lastShot = 0;
+        }else{
+            lastShot++;
+        }
+        for(int i = 0; i < getLevel().getBullets().size(); i++){
+            if(getLevel().getBullets().get(i).getX() == getX() + 100){
+                setHealth(getHealth() - 1);
+                getLevel().getBullets().remove(i);
+            }
+        }
     }
 
     @Override
